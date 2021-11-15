5194f2a84c1160c26cdbaea556ecdb11286a9f6a
Gaston Caminiti <gastoncaminiti@gmail.com>
2021-11-15 13:27:47 -0300

    Commit 0 [ESTRUCTURA INICIAL DEL PROYECTO]
    Proyecto Unity
    .gitignore
    .readme
    Models Foods

---
+++ CLASE 14 - TDA Y BUCLES/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private int lifePlayer = 3;
+    [SerializeField] private float cameraAxisX = -90f;
+    [SerializeField] private float speedPlayer = 3f;
+    [SerializeField] private float speedTurn= 7f;
+
+    [SerializeField] private Animator animPlayer;
+    [SerializeField] private AudioClip punchSound;
+    [SerializeField] private AudioClip walkSound;
+    [SerializeField] private GameObject mPlayer;
+
+    private AudioSource audioPlayer;
+    private Rigidbody rbPlayer;
+
+    private static string playerName = "VAMPIRO X";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(PlayerController.playerName);
+        animPlayer.SetBool("isRun", false);
+        audioPlayer = GetComponent<AudioSource>();
+        rbPlayer    = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            audioPlayer.PlayOneShot(punchSound, 1f);
+            animPlayer.SetBool("isPunch", true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            animPlayer.SetBool("isPunch", false);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+        RotatePlayer();
+    }
+
+    private void Move()
+    {
+        float ejeHorizontal = Input.GetAxis("Horizontal");
+        float ejeVertical   = Input.GetAxis("Vertical");
+
+        if (ejeHorizontal != 0 || ejeVertical != 0) {
+            animPlayer.SetBool("isRun", true);
+            rbPlayer.AddRelativeForce(Vector3.forward * speedPlayer * ejeVertical, ForceMode.Force);
+            rbPlayer.AddRelativeForce(Vector3.right  * speedPlayer * ejeHorizontal, ForceMode.Force);
+            if (!audioPlayer.isPlaying)
+            {
+                audioPlayer.PlayOneShot(walkSound, 0.5f);
+            }
+        }
+        else
+        {
+            animPlayer.SetBool("isRun", false);
+        }
+    }
+    private void RotatePlayer()
+    {
+        if(Input.GetAxis("Mouse X") != 0) { 
+            cameraAxisX += Input.GetAxis("Mouse X");
+            Quaternion angulo   = Quaternion.Euler(0, cameraAxisX * speedTurn, 0);
+            transform.rotation = angulo;
+            mPlayer.transform.rotation = transform.rotation;
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            lifePlayer--;
+            Destroy(collision.gameObject);
+            if(lifePlayer < 0)
+            {
+                Debug.Log("GAME OVER");
+            }
+        }
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Generator"))
+        {
+            other.gameObject.GetComponent<GeneratorController>().setNewColor(Color.black);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# REPOSITORIO CLASE 13 - DESARROLLO DE VIDEOJUEGOS
+
+Proyecto desarrollado en el curso de desarrollo de videojuego de CoderHouse.
+
+Tema: Patrón de programación: Singleton - Raycasting
+
+Engine: Unity
